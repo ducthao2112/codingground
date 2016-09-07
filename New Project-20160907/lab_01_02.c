94212c3f3b31aafc7454e11deead8c71ae03f998
ducthao2112 <ducthao2112@gmail.com>
2016-09-07 13:49:49 +0200

    Created by Coding Ground Client

---
+++ New Project-20160907/lab_01_02.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+        char    c1 = 110, 
+                c2 = 't';
+        printf("c1 = %d \n", c1);
+        printf("c1 = %d \n", c1);
+        printf("c2 = %d \n", c2);
+        const int coef = 2016;
+        double  a = 1005.23125478965,
+                b = 5.347;
+        printf("%7d \n", coef);
+        printf("%7.11f \n", b);
+        printf("A = a : %.7f \n", a);
+        printf("B = b : %.3f \n", b);
+        printf("%5d.7f \n", a);
+        printf("%7d.1f \n", b);
+        printf("%7d \n", coef);
+}
+
+
