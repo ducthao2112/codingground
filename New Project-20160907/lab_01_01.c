57745fa7f827c891e3fac34f5df05772588b2836
ducthao2112 <ducthao2112@gmail.com>
2016-09-07 13:53:13 +0200

    Created by Coding Ground Client

---
+++ New Project-20160907/lab_01_01.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void main() {
+        
+        int x = 13;
+        int y = 4;
+        printf("x + y = %d \n", x + y);
+        printf("x - y = %d \n", x - y);
+        printf("x * y = %d \n", x * y);
+        printf("x %% y = %d \n", x % y);
+        printf("x / y = %d \n", x / y);
+        double  a = 11.7,
+                b = 3.5;
+        printf("a / b = %lf \n", a / b);
+        printf("x + a = %lf \n", x + a);
+        unsigned char   c1 = 10,
+                        c2 = 200;
+        printf("c1 + c2 = %d \n", c1 + c2);
+        int i = 15;
+        printf("i = %d\n", i);
+        i = i + 1;
+        printf("i=i+1 : %d \n", i);
+        i++;
+        printf("i++ : %d \n", i);
+        i += 1;
+        printf("i+=1 : %d \n", i);
+        
+}
